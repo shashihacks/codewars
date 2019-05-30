9a44c73686957375830f83821233dab67d22a052
Shashi Kumar <shashisoft@outlook.com>
2019-05-30 15:23:21 +0200

    digital_root 6 kyu

---
+++ digital_root.js
@@ -0,0 +1,17 @@
+function digital_root(n) {
+  let numArray = []
+    n = n.toString().split('').forEach(element => {
+      numArray.push(parseInt(element))
+    });
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    while(numArray.length>1) {
+      n = numArray.reduce(reducer).toString().split('');
+      numArray = [];
+      n.forEach(element => {
+        numArray.push(parseInt(element))
+      });
+    }
+   return (numArray.reduce(reducer));  
+}
+
+console.log(digital_root(132189)) //2
